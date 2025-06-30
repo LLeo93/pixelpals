f4441e3d7c579129845753921481f20f6f9d5d49
LLeo93 <liba.leoncini@gmail.com>
2025-06-30 11:12:01 +0200

    inizializzazione backend springboot

---
+++ backend/src/main/java/com/pixelpals/backend/PixelpalsApplication.java
@@ -0,0 +1,13 @@
+package com.pixelpals.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelpalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelpalsApplication.class, args);
+	}
+
+}
